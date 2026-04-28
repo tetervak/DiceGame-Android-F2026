b9f2d65f10aefc95ad27b424da51512a909c982d
Alex Tetervak <tetervak@sheridancollege.ca>
2026-04-28 09:43:28 -0400

    added roller screen previews

--- app/src/main/java/ca/tetervak/dicegame/ui/roller/RollerScreen.kt
+++ app/src/main/java/ca/tetervak/dicegame/ui/roller/RollerScreen.kt
@@ -23,10 +23,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ca.tetervak.dicegame.R
+import ca.tetervak.dicegame.domain.RollData
 import ca.tetervak.dicegame.ui.common.DiceGameAbout
 import ca.tetervak.dicegame.ui.common.GameTopAppBar
+import ca.tetervak.dicegame.ui.theme.DiceGameTheme
+import java.time.LocalDateTime
+import java.util.Date
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -35,7 +40,8 @@ fun RollerScreen(
     onChangeNumberOfDice: (Int) -> Unit,
     onRoll: () -> Unit,
     onReset: () -> Unit,
-){
+    modifier: Modifier = Modifier
+) {
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
     var showAboutDialog: Boolean by rememberSaveable {
         mutableStateOf(false)
@@ -57,7 +63,7 @@ fun RollerScreen(
                 )
             }
         },
-        modifier = Modifier
+        modifier = modifier
             .fillMaxSize()
             .nestedScroll(scrollBehavior.nestedScrollConnection)
     ) { innerPadding ->
@@ -104,4 +110,34 @@ fun RollerScreen(
         DiceGameAbout(onDismissRequest = { showAboutDialog = false })
     }
 }
+
+@Preview
+@Composable
+fun RollerScreenNotRolledPreview() {
+    DiceGameTheme {
+        RollerScreen(
+            uiState = RollerUiState.NotRolled(2),
+            onChangeNumberOfDice = {},
+            onRoll = {},
+            onReset = {}
+        )
+    }
+}
+
+@Preview
+@Composable
+fun RollerScreenRolledPreview() {
+    DiceGameTheme {
+        RollerScreen(
+            uiState = RollerUiState.Rolled(
+                rollData = RollData(listOf(2, 3, 4)),
+                date = Date()
+            ),
+            onChangeNumberOfDice = {},
+            onRoll = {},
+            onReset = {}
+        )
+    }
+}
+
 
