8faf42214ce27e6ea366456f16af91090147ed60
Alex Tetervak <tetervak@sheridancollege.ca>
2026-04-28 20:26:50 -0400

    simplified random list generation

--- app/src/main/java/ca/tetervak/dicegame/repository/RollDataRepository.kt
+++ app/src/main/java/ca/tetervak/dicegame/repository/RollDataRepository.kt
@@ -12,16 +12,7 @@ class RollDataRepository(
     constructor(): this(Random.Default)
 
     fun getRandomRollData(numberOfDice: Int): RollData {
-        if (numberOfDice > 0) {
-            val list: List<Int> =
-                buildList(capacity = numberOfDice) {
-                    repeat(numberOfDice) {
-                        add(random.nextInt(from = 1, until = 7))
-                    }
-                }
-            return RollData(list)
-        } else {
-            throw IllegalArgumentException("Illegal numberOfDice = $numberOfDice")
-        }
+        if (numberOfDice <= 0) throw IllegalArgumentException("numberOfDice must be > 0")
+        return RollData(List(numberOfDice) { random.nextInt(1, 7) })
     }
 }
